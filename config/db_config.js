03bb177c790a89b8c30ec16d4aac68f6ea0b1753
KasperHovlandRasmussen <3803976@rts-365.dk>
2018-02-22 15:05:59 +0100

    Rettelse i db_config

--- config/db_config.js
+++ config/db_config.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-	host: '139.59.156.136',
-	user: 'admin',
-	password: 'admin',
+	host: 'localhost',
+	user: 'root',
+	password: '',
 	database: 'frisor'
 });
 
